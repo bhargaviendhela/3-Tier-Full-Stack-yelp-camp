4f6f2873bac7f129305f777515cbbef2e03129cb
bhargaviendhela <bhargavireddyendhela@gmail.com>
2026-06-04 12:11:00 +0530

    Update app.js

--- app.js
+++ app.js
@@ -91,16 +91,8 @@ const scriptSrcUrls = [
 const styleSrcUrls = [
     "https://kit-free.fontawesome.com/",
     "https://stackpath.bootstrapcdn.com/",
-    "https://api.mapbox.com/",
-    "https://api.tiles.mapbox.com/",
     "https://fonts.googleapis.com/",
     "https://use.fontawesome.com/",
-];
-const connectSrcUrls = [
-    "https://api.mapbox.com/",
-    "https://a.tiles.mapbox.com/",
-    "https://b.tiles.mapbox.com/",
-    "https://events.mapbox.com/",
 ];
 const fontSrcUrls = [];
 
